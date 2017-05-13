ae28b58e57bce46d6596f46cf43119e31a180ce5
Rizwan Rahman Renesa <rrenesa@localhost.localdomain>
2017-05-13 20:17:28 +1200

    added copy

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -3,7 +3,11 @@ var React = require('react');
 var Example = React.createClass({
   render:function () {
     return (
+      <div>
          <h3>Example Component</h3>
+         <p>Welcome to examples page !</p>
+        </div>
+
 
     )
   }
