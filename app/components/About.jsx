d3f6deb2a99964770a2dc2a0324519788d248b0e
Rizwan Rahman Renesa <rrenesa@localhost.localdomain>
2017-05-16 20:36:49 +1200

    changes in the About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,9 +4,20 @@ var About = React.createClass({
   render:function () {
     return (
         <div>
-         <h3>About</h3>
-         <p>Welcome to About page</p>
-         </div>
+         <h1 className ="text-center">About</h1>
+         <p>This is the weather application build on React.I have build this for the complete React Web app Developer course</p>
+         <p>Here are some of the tools I used:</p>
+         <ul>
+           <li>
+             <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+           </li>
+           <li>
+ <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather by city name.
+
+           </li>
+         </ul>
+
+     </div>
 
     )
   }
--- public/bundle.js
+++ public/bundle.js
@@ -28717,26 +28717,55 @@
 /* 258 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
 	var About = React.createClass({
-	  displayName: 'About',
+	  displayName: "About",
 
 	  render: function render() {
 	    return React.createElement(
-	      'div',
+	      "div",
 	      null,
 	      React.createElement(
-	        'h3',
-	        null,
-	        'About'
+	        "h1",
+	        { className: "text-center" },
+	        "About"
 	      ),
 	      React.createElement(
-	        'p',
+	        "p",
 	        null,
-	        'Welcome to About page'
+	        "This is the weather application build on React.I have build this for the complete React Web app Developer course"
+	      ),
+	      React.createElement(
+	        "p",
+	        null,
+	        "Here are some of the tools I used:"
+	      ),
+	      React.createElement(
+	        "ul",
+	        null,
+	        React.createElement(
+	          "li",
+	          null,
+	          React.createElement(
+	            "a",
+	            { href: "https://facebook.github.io/react" },
+	            "React"
+	          ),
+	          " - This was the JavaScript framework used."
+	        ),
+	        React.createElement(
+	          "li",
+	          null,
+	          React.createElement(
+	            "a",
+	            { href: "http://openweathermap.org" },
+	            "Open Weather Map"
+	          ),
+	          " - I used Open Weather Map to search for weather by city name."
+	        )
 	      )
 	    );
 	  }
